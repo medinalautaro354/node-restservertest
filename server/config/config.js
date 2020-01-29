11f8061e6cedad61fa27cc99bf452ab5ee8551ed
Leo <lautaroleonel.medina@terrand-audits.com>
2020-01-28 21:31:04 -0300

    variable de entorno MONGO URI

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let connectionString;
 if (process.env.NODE_ENV === 'dev') {
     connectionString = 'mongodb://localhost:27017/cafe';
 } else {
-    connectionString = process.env.NODE_ENV;
+    connectionString = process.env.MONGO_URI;
 }
 
 process.env.ConnectionString = connectionString;

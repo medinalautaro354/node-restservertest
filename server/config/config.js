fc2801909cf279202341a0852fc0305cdc97eacc
Leo <lautaroleonel.medina@terrand-audits.com>
2020-01-28 21:29:02 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let connectionString;
 if (process.env.NODE_ENV === 'dev') {
     connectionString = 'mongodb://localhost:27017/cafe';
 } else {
-    connectionString = 'mongodb+srv://test:guyKLvABrn9qnx5g@cluster0-kxpb8.mongodb.net/test';
+    connectionString = process.env.NODE_ENV;
 }
 
 process.env.ConnectionString = connectionString;
